463f170ecde57452d4a34dbe083908e726fa5ef3
Matvey <matthew123return@gmail.com>
2026-01-24 18:23:35 +0300

    Delete bin directory

--- bin/main/ru/yarsu/cli/commands/BaseCmd.kt
+++
@@ -1,9 +0,0 @@
-package ru.yarsu.cli.commands
-
-import com.beust.jcommander.ParametersDelegate
-import ru.yarsu.cli.args.PathArgs
-
-open class BaseCmd {
-    @ParametersDelegate
-    val path = PathArgs()
-}
--- bin/main/ru/yarsu/web/routes/util/PaginationUtils.kt
+++
@@ -1,9 +0,0 @@
-package ru.yarsu.web.routes.util
-
-object PaginationUtils {
-    fun <T> applyPaging(
-        items: List<T>,
-        page: Int,
-        perPage: Int,
-    ): List<T> = items.drop((page - 1) * perPage).take(perPage)
-}
